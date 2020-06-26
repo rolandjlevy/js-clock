0ac74d2901bc62148cf0c185f0abc81b2624632c
Roland Levy <rolandjlevy@gmail.com>
2020-06-26 00:09:30 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', (e) => {
+
+  const clock = document.querySelector('.digital-clock');
+  const clockMs = document.querySelector('.digital-clock-ms');
+  const milliSecondsArm = document.querySelector('.arm.milliseconds');
+  const secondsArm = document.querySelector('.arm.seconds');
+  const minutesArm = document.querySelector('.arm.minutes');
+  const hoursArm = document.querySelector('.arm.hours');
+
+  const MS = 1;
+  const SECS = 1000;
+  const MINS = 60 * 1000;
+  const MILLINC = 360 / 1000
+
+  const milliSeconds = () => {
+    const ms = new Date().getMilliseconds();
+    clockMs.innerHTML = ms.toString().padStart(3, '0');
+    const millDegrees = Math.round(Number(ms) * MILLINC);
+    milliSecondsArm.style.transform = `rotate(${millDegrees}deg)`;
+  }
+  setInterval(milliSeconds, MS);
+
+  const seconds = () => {
+    const hours = new Date().getHours();
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    const hoursStr = (hours % 12).toString().padStart(2, '0');
+    const mins = new Date().getMinutes().toString().padStart(2, '0');
+    const secs = new Date().getSeconds().toString().padStart(2, '0');
+    clock.innerHTML = `${hoursStr}:${mins}:${secs}:`;
+    secondsArm.style.transform = `rotate(${6 * Number(secs)}deg)`;
+    minutesArm.style.transform = `rotate(${6 * Number(mins)}deg)`;
+    hoursArm.style.transform = `rotate(${30 * Number(hours)}deg)`;
+  }
+  seconds();
+  setInterval(seconds, SECS);
+
+});

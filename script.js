da21a602439d1ddfad66de20691655ddd511d2e5
Roland Levy <rolandjlevy@gmail.com>
2020-12-25 23:46:50 +0000

    Fixed millisecond lag issue

--- script.js
+++ script.js
@@ -1,30 +1,20 @@
-const clock = document.querySelector('.digital-clock');
-const clockMs = document.querySelector('.digital-clock-ms');
-const milliSecondsArm = document.querySelector('.arm.milliseconds');
-const secondsArm = document.querySelector('.arm.seconds');
-const minutesArm = document.querySelector('.arm.minutes');
-const hoursArm = document.querySelector('.arm.hours');
+const $ = (selector) => document.querySelector(selector);
 
 const MS = 1, SECS = 1000, MINS = 60 * 1000, MILLINC = 360 / 1000
 
-const milliSeconds = () => {
+const runClock = () => {
   const ms = new Date().getMilliseconds();
-  clockMs.innerHTML = ms.toString().padStart(3, '0');
   const millDegrees = Math.round(Number(ms) * MILLINC);
-  milliSecondsArm.style.transform = `rotate(${millDegrees}deg)`;
-}
-setInterval(milliSeconds, MS);
-
-const seconds = () => {
-  const hours = new Date().getHours();
-  const ampm = hours >= 12 ? 'PM' : 'AM';
-  const hoursStr = (hours % 12).toString().padStart(2, '0');
-  const mins = new Date().getMinutes().toString().padStart(2, '0');
   const secs = new Date().getSeconds().toString().padStart(2, '0');
-  clock.innerHTML = `${hoursStr}:${mins}:${secs}:`;
-  secondsArm.style.transform = `rotate(${6 * Number(secs)}deg)`;
-  minutesArm.style.transform = `rotate(${6 * Number(mins)}deg)`;
-  hoursArm.style.transform = `rotate(${30 * Number(hours)}deg)`;
+  const mins = new Date().getMinutes().toString().padStart(2, '0');
+  const hours = new Date().getHours();
+  const hoursStr = (hours % 12).toString().padStart(2, '0');
+  $('.digital-clock').innerHTML = `${hoursStr}:${mins}:${secs}:`;
+  $('.digital-clock-ms').innerHTML = ms.toString().padStart(3, '0');
+  $('.arm.milliseconds').style.transform = `rotate(${millDegrees}deg)`;
+  $('.arm.seconds').style.transform = `rotate(${6 * Number(secs)}deg)`;
+  $('.arm.minutes').style.transform = `rotate(${6 * Number(mins)}deg)`;
+  $('.arm.hours').style.transform = `rotate(${30 * Number(hours)}deg)`;
 }
-seconds();
-setInterval(seconds, SECS);
+
+setInterval(runClock, MS);

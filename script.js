632ff9d02b1621c3052898b9187ceb76bb11e0b0
Roland Levy <rolandjlevy@gmail.com>
2020-12-26 00:43:34 +0000

    Refactored

--- script.js
+++ script.js
@@ -7,17 +7,19 @@ $$('.arm').forEach(item => item.classList.add('init'));
 
 const runClock = () => {
   const ms = new Date().getMilliseconds();
-  const millDegrees = Math.round(Number(ms) * MILLINC);
   const secs = new Date().getSeconds().toString().padStart(2, '0');
   const mins = new Date().getMinutes().toString().padStart(2, '0');
   const hours = new Date().getHours();
+  
   const hoursStr = (hours % 12).toString().padStart(2, '0');
   $('.digital-clock').innerHTML = `${hoursStr}:${mins}:${secs}:`;
   $('.digital-clock-ms').innerHTML = ms.toString().padStart(3, '0');
+
+  const millDegrees = Math.round(Number(ms) * MILLINC);
+  const accurateHour = hours + parseFloat(mins / 60) + parseFloat(secs / 3600);
   $('.arm.milliseconds').style.transform = `rotate(${millDegrees}deg)`;
   $('.arm.seconds').style.transform = `rotate(${6 * Number(secs)}deg)`;
   $('.arm.minutes').style.transform = `rotate(${6 * Number(mins)}deg)`;
-  const accurateHour = hours + parseFloat(mins / 60) + parseFloat(secs / 3600);
   $('.arm.hours').style.transform = `rotate(${15 * accurateHour}deg)`;
 }
 

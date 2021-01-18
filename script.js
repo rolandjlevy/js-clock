832848e8576a0ad446bf860ddab129fafb0d33e5
Roland Levy <rolandjlevy@gmail.com>
2021-01-18 19:42:51 +0000

    CSS tweaks

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 const $ = (selector) => document.querySelector(selector);
-const $$ = (selector) => document.querySelectorAll(selector);
 const speed = 15, millInc = 360 / 1000;
-
-$$('.arm').forEach(item => item.classList.add('init'));
 
 $('#year').textContent = new Date().getFullYear();
 
@@ -12,8 +9,8 @@ const runClock = () => {
   const mins = new Date().getMinutes().toString().padStart(2, '0');
   const hours = new Date().getHours();
   const hoursStr = (hours).toString().padStart(2, '0');
-  $('.digital-clock').innerHTML = `${hoursStr}:${mins}:${secs}:`;
-  $('.digital-clock-ms').innerHTML = ms.toString().padStart(3, '0');
+  $('.digital-clock').textContent = `${hoursStr}:${mins}:${secs}:`;
+  $('.digital-clock-ms').textContent = ms.toString().padStart(3, '0');
   const millDegrees = Math.round(Number(ms) * millInc);
   const accurateHour = hours + parseFloat(mins / 60) + parseFloat(secs / 3600);
   $('.arm.milliseconds').style.transform = `rotate(${millDegrees}deg)`;
